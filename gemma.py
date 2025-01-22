4d3dd508ad4db2512edbcabcf3b25e2631090a81
Muhammad Salman <salmankhattak021@gmail.com>
2025-01-22 23:16:52 +0500

    first commit

--- gemma.py
+++ gemma.py
@@ -7,7 +7,7 @@ from langchain.agents.agent_types import AgentType
 from langchain.agents import Tool, initialize_agent
 from langchain.callbacks import StreamlitCallbackHandler
 
-# Set upi the Stramlit app
+## Set upi the Stramlit app
 st.set_page_config(page_title="Text To MAth Problem Solver And Data Serach Assistant",page_icon="🧮")
 st.title("Text To Math Problem Solver Uing Google Gemma 2")
 
